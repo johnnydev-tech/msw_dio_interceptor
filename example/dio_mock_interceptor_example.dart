a908220212bb53b3e42768235fdc7ec5dbc5b118
johnnydev-tech <johnnyfreirejh@hotmail.com>
2025-11-13 20:02:37 -0300

    first commit

---
+++ example/dio_mock_interceptor_example.dart
@@ -0,0 +1,6 @@
+import 'package:dio_mock_interceptor/dio_mock_interceptor.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dio_mock_interceptor.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/dio_mock_interceptor_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
